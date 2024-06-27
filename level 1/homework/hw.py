ea05c6b00ae1fbb69834a5287530c5cbf63bda7a
777GiorgiGiorgobiani <george.learning.programming@gmail.com>
2024-06-27 18:51:41 +0400

    first commit

---
+++ level 1/homework/hw.py
@@ -0,0 +1,18 @@
+# # data type = 1,"...",7.5
+
+# name = "Gio" # string - str
+# age = 17 # integer - int
+# bthday = 7.4  # float - float
+
+# print(name)
+
+# name = 17
+# name2 = 17
+# print(name + name2)
+# print(str(17) + str(17))     # str(17) ->  "17"      # "Witeli" + "manqana" = "witelimanqana"  # "17" + "17" = "1717"
+# print(int("17") + int("17"))    # "17" int("17") -> 17       
+
+
+
+# print(type("HELLO WORLD!"))
+# print(type(17))
