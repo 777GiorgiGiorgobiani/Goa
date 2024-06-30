a0496a69564df9b72e84625c59f6e0dcf6c1bbe1
777GiorgiGiorgobiani <george.learning.programming@gmail.com>
2024-06-30 16:17:10 +0400

    third commit

---
+++ level 3/homework/hw.py
@@ -0,0 +1,15 @@
+# user's info
+name = input("what is your name: ")
+country = input("where are you from: ")
+age = input("how old are you: ")
+hobbies = input("what hobbies do you enjoy: ")
+interests = input("what are you interested in: ")
+music = input("what kind of music do you listen to: ")
+
+# output
+print("Hello! my name is", name + ",",
+      "I am from", country + ",",
+        "I am", age, "years old,",
+        "my hobbies are/is", hobbies + ",",
+        "I am really interested in", interests + ",",
+        "I really like listening to", music + ".")
