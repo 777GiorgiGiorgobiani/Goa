b0644f4d354cb8bf6820bda194ea261b6cdc0990
777GiorgiGiorgobiani <george.learning.programming@gmail.com>
2024-07-14 11:47:27 +0400

    7th commit

--- level 7/homework/hw7.py
+++ level 7/homework/hw7.py
@@ -1,46 +1,41 @@
 # 7) გაიმეორეთ გავლილი მასალა
 
-# # if elif else
+# if elif else
 
-# health = 0
+health = 0
 
-# if health == 0:
-#     print("You've died")
-# elif health <= 25:
-#     print("you are near death")
-# else:
-#     print("you are alive")
-
-
-
-# # sequence and random
-
-# from random import *
-
-
-# buildings = ["Bank", "Hospital", "Pharmacy", "Mall", "Police Station", "Apartment Complex"]
-# rng_buildings = randint(0, 5)
-# print(buildings[rng_buildings])
+if health == 0:
+    print("You've died")
+elif health <= 25:
+    print("you are near death")
+else:
+    print("you are alive")
 
 
 
-# # for loop
+# sequence and random
 
-# buildings = ["Bank", "Hospital", "Pharmacy", "Mall", "Police Station", "Apartment Complex"]
-# for i in range(len(buildings)):
-#     print(buildings[i])
+from random import *
 
 
-
-# # while loop
-
-# full_bottle = 100
-# empty_bottle = 0
-# while full_bottle > empty_bottle:
-#     full_bottle = full_bottle - 25
-#     print(full_bottle)
+buildings = ["Bank", "Hospital", "Pharmacy", "Mall", "Police Station", "Apartment Complex"]
+rng_buildings = randint(0, 5)
+print(buildings[rng_buildings])
 
 
-x = 9
-y = 3.0
-print(x+y)
+
+# for loop
+
+buildings = ["Bank", "Hospital", "Pharmacy", "Mall", "Police Station", "Apartment Complex"]
+for i in range(len(buildings)):
+    print(buildings[i])
+
+
+
+# while loop
+
+full_bottle = 100
+empty_bottle = 0
+while full_bottle > empty_bottle:
+    full_bottle = full_bottle - 25
+    print(full_bottle)
