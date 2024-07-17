1581ba5752ad85b1f2b1e65032f7670e95e8d89e
777GiorgiGiorgobiani <george.learning.programming@gmail.com>
2024-07-18 00:29:14 +0400

    8th commit

---
+++ level 8/classwork/cw.py
@@ -0,0 +1,25 @@
+# Compare - შედარება
+# Comparison operations - შედარებითი ოპერაცია, სიმბოლოები: < ; >
+# true - ჭეშმარიტი/სწორი
+# false - მცდარი/არასწირე
+# boolean - დატას ტიპი, აქვს მნიშვნელობა ჭეშმარიტი ან მცდარი (მხოლოდ ეს ორი) true/false
+
+print(5 < 9)    # True
+print(5 > 9)    # False
+print("7")      # str
+print(7)        # int
+print(7.0)      # float
+
+# statement - სტატია/სთქმულება
+
+# circut - წრედი
+
+
+# binary code system = ორობითი სისტემა ex: 0111001001
+# morse code (პროგრამირებაში არ გამოიყენება) - ex: _..__.....__._____._._....._____..._.
+
+
+print(type(True))
+print(type(False))
+print(5 > 0)
+print(5 < 0)
